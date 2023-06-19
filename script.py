5245a781017562b06b3d746bad10368de672b135
Antimony051 <72787497+Antimony051@users.noreply.github.com>
2023-06-19 15:10:40 +0000

    Update script.py
    fixed bugs

--- script.py
+++ script.py
@@ -8,8 +8,15 @@ import utils
 path_input=input("enter the path to extracted Mydata file: ")
 out_file_path=fmanager.mange_files(path_input)
 
-json_handler(out_file_path)
+def json_handler(f_name):
+    current_file= open(f_name)
+    return(json.load(current_file))
+    current_file.close()
 
-artists= utils.get_all("master_metadata_album_artist_name")
-songs= utils.get_all("master_metadata_track_name")
-albums= utils.get_all("master_metadata_track_name")
+json_data=json_handler(out_file_path)
+
+artists= utils.get_all("master_metadata_album_artist_name",json_data)
+songs= utils.get_all("master_metadata_track_name",json_data)
+albums= utils.get_all("master_metadata_track_name",json_data)
+
+print(artists.top_10())
